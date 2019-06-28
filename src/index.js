7216cecae9c952bd96012fa550c4f635590daa63
LucSaunders <lucsaunders@gmail.com>
2019-06-28 03:53:07 -0400

    First commit, with initial dependencies and Apollo set-up in index.js.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+const GITHUB_BASE_URL = 'https://api.github.com/graphql';
+
+const httpLink = new HttpLink({
+  uri: GITHUB_BASE_URL,
+  headers: {
+    authorization: `Bearer ${process.env.REACT_APP_PERSONAL_ACCESS_TOKEN}`
+  }
+});
+
+const cache = new InMemoryCache();
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache
+});
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
