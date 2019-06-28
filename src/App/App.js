fe8bde563a005232c2a7058596e858de11d4c8d3
LucSaunders <lucsaunders@gmail.com>
2019-06-28 04:12:07 -0400

    Clean up cra boilerplate. Add basic file structure.

---
+++ src/App/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <p>Initial set-up for gh-tracker-gql *In PROGRESS*</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ApolloProvider } from 'react-apollo';
 import { ApolloClient } from 'apollo-client';
 import { HttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
-import App from './App';
+import App from './App/App';
 import * as serviceWorker from './serviceWorker';
+
+import './index.css';
 
 const GITHUB_BASE_URL = 'https://api.github.com/graphql';
 
